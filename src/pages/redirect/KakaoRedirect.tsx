e0a382efd9f06910b194dab69e52c1c0606e6e49
unknown <crystal5k123@gmail.com>
2025-08-08 20:49:29 +0900

    무한루프 해결

--- src/pages/redirect/KakaoRedirect.tsx
+++ src/pages/redirect/KakaoRedirect.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useModalContext } from '../../components/Modal/context/ModalContext';
 import ConsentModal from '../login/components/ConsentModal';
 import styled from 'styled-components';
@@ -6,11 +6,19 @@ import axiosInstance from '../../apis/axiosInstance';
 
 function KakaoRedirect() {
   const { openModal } = useModalContext();
+  const hasProcessed = useRef(false);
 
   useEffect(() => {
     console.log('=== KakaoRedirect useEffect 실행됨 ===');
     console.log('현재 URL:', window.location.href);
     console.log('현재 쿠키:', document.cookie);
+    console.log('이미 처리됨:', hasProcessed.current);
+    
+    // 이미 처리되었다면 중복 실행 방지
+    if (hasProcessed.current) {
+      console.log('이미 처리됨 - 중복 실행 방지');
+      return;
+    }
     
     const handleKakaoLogin = async () => {
       try {
@@ -26,10 +34,12 @@ function KakaoRedirect() {
         if (response.data.result === 'user') {
           // 기존 사용자 - 홈으로 리다이렉트
           console.log('기존 사용자 - 홈으로 리다이렉트');
+          hasProcessed.current = true;
           window.location.href = '/';
         } else {
           // 신규 사용자 - 약관 동의 모달 열기
           console.log('신규 사용자 - 약관 동의 모달 열기');
+          hasProcessed.current = true;
           openModal(({ onClose }) => <ConsentModal onClose={onClose} />);
         }
         
@@ -38,6 +48,7 @@ function KakaoRedirect() {
         
         // 에러가 발생해도 신규 사용자로 간주하고 약관 동의 모달 열기
         console.log('에러 발생으로 인해 신규 사용자로 간주, 약관 동의 모달 열기');
+        hasProcessed.current = true;
         openModal(({ onClose }) => <ConsentModal onClose={onClose} />);
       }
     };
@@ -46,7 +57,7 @@ function KakaoRedirect() {
     setTimeout(() => {
       handleKakaoLogin();
     }, 1000);
-  }, [openModal]);
+  }, []); // openModal을 의존성 배열에서 제거
 
   return <Container></Container>;
 }
